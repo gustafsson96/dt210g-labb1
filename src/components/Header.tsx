e79e85453ca3c2866f51a0e598d162d1c92dfacd
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-01-21 18:08:56 +0100

    Add banner image to header and style title and banner using inline styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import banner from "../assets/images/banner.jpg"
+
+// Header containing title and banner image, both with inline styling
 function Header({title}: { title: string}) {
     return (
         <>
             <header>
-                <h1>{title}</h1>
-                <img src="" alt="" />
+                <h1 style={{textAlign: "center", fontSize: "2.5em", fontFamily: "Tahoma, Verdana, Segoe UI, Arial, sans-serif" }}>{title}</h1>
+                <img src={banner} alt="Tjej med hantlar som gör en tricepsövning" style={{ width: "100%", height: "auto" }} />
             </header>
         </>
     )
