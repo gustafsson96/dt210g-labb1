b232e6b5637663ef9214251f1d87650efd728c94
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-01-21 19:58:21 +0100

    Add comments to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer"
 
 function App() {
 
+  // Display components for header (with Övningsbank as prop for title), section with exercise cards and footer
   return (
     <>
       <Header title="Övningsbank" />
