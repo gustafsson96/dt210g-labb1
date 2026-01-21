abfbfc73e78483cfb50599c043e88ad07dfd01c1
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-01-21 11:57:02 +0100

    Delete test file and create component for exercise cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import Test from "./components/Test"
-
 function App() {
 
   return (
     <>
-      <Test />
     </>
   )
 }
---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,13 @@
+function ExerciseCard() {
+  return (
+    <div>
+      <h2>Övning</h2>
+      <p>Sets x Reps</p>
+      <p>Muskelgrupp</p>
+      <p>Kräver utrustning</p>
+    </div>
+  )
+}
+
+export default ExerciseCard
+
