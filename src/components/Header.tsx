f3da064c2eb2741de8a873bc2d0421a41eb0327a
Julia Gustafsson <juliagustafsson96@hotmail.com>
2026-01-21 15:45:38 +0100

    Create basic structure for header compontnet

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <>
+            <header>
+                <h1></h1>
+                <img src="" alt="" />
+            </header>
+        </>
+    )
+}
+
+export default Header
+
